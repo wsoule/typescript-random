1bd39a0d34b36bd60b2ceb3293eeb20b20c3f80f
Wyat Soule <wsoule679@gmail.com>
2022-09-29 23:41:06 -0400

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,6 @@
+let message: string = 'Hello mother!';
+// create new heading 1 element
+let heading = document.createElement('h1');
+heading.textContent = message;
+// add the heading to the document
+document.body.appendChild(heading);
